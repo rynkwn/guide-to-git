71d251b6fcc4d3e573b389034d127620dcb07168
Ryan Kwon <rynkwn@gmail.com>
2018-09-16 15:56:04 -0700

    Added some color variables.

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-clear
+BLACK='\033[0;30m'
+GREEN='\033[0;32m'
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+WHITE='\033[1;37m'
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
@@ -20,8 +25,9 @@ function start_level () {
 	echo; echo;
 }
 
+clear
 if [ $# -eq 0 ]; then
-	cat ${DIR}/res/raws/start.txt
+	printf "%s" "$(<${DIR}/res/raws/start.txt)"
 else
 	start_level $1
 fi
